09174f2a7ce5631647ec9c20a290039e30e3ebbe
F4_IT_Intership_QuocDat.Dang <quocdat.dang@spclt.com.vn>
2025-05-26 09:43:57 +0700

    init

---
+++ Export_Data/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Timers;
+using System.IO;
+
+namespace Export_Data
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer exportTimer;
+
+        private string connectionString = "Data Source=10.4.17.184;Initial Catalog=DEMO;User ID=tk2;Password=123456;Integrated Security=False;";
+        private string exportFolder = @"D:\Desktop\Practice\";
+        private DataTable currentData;
+        public Form1()
+        {
+            InitializeComponent();
+            exportTimer = new System.Timers.Timer();
+            exportTimer.Interval = 60 * 60 * 1000; // 1 giờ = 3600000 ms
+            exportTimer.Elapsed += ExportTimer_Elapsed;
+            exportTimer.Start();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MessageBox.Show("Kết nối thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kết nối thất bại: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentData = FetchDataFromSql();
+            dataGridView1.DataSource = currentData;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentData != null && currentData.Rows.Count > 0)
+            {
+                ExportDataToTxt(currentData);
+                MessageBox.Show("Đã xuất dữ liệu ra file .txt thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Không có dữ liệu để xuất.", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void ExportTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DataTable data = FetchDataFromSql();
+            ExportDataToTxt(data);
+        }
+        private DataTable FetchDataFromSql()
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT TOP 50 * FROM View_1";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi truy vấn dữ liệu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return dt;
+        }
+        private void ExportDataToTxt(DataTable dt)
+        {
+            try
+            {
+                string fileName = $"Export_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+                string filePath = Path.Combine(exportFolder, fileName);
+                StringBuilder sb = new StringBuilder();
+
+                // Header
+                foreach (DataColumn col in dt.Columns)
+                {
+                    sb.Append(col.ColumnName + "\t");
+                }
+                sb.AppendLine();
+
+                // Data
+                foreach (DataRow row in dt.Rows)
+                {
+                    foreach (var item in row.ItemArray)
+                    {
+                        sb.Append(item.ToString() + "\t");
+                    }
+                    sb.AppendLine();
+                }
+
+                File.WriteAllText(filePath, sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi xuất file: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
